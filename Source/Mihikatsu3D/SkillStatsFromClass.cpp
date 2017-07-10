8695a1f5231f425cc4892d6fc708d62f38caf1e2
Jakub Zarebski <kz.zarebski11@gmail.com>
2017-07-10 23:05:39 +0200

    Prototype 0.0.2
    Added:
     - After each level gains player will get flat increase in all primal stats : +25 to HP/MP and +10 to Stamina
       - Number arent final
     - Added new buttons for new skills
     - Names in button now are proper name of skills
    Fixes:
     - Fixes to buttons
     - Stamina didnt recharge to new maximum now it does
     - UI stats bar now take maximum of set stats to display decrease properly

---
+++ Source/Mihikatsu3D/SkillStatsFromClass.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SkillStatsFromClass.h"
+
+SkillStatsFromClass::SkillStatsFromClass()
+{
+}
+
+SkillStatsFromClass::~SkillStatsFromClass()
+{
+}
